85840d3d1b6281b0c45400820403ae830edbba55
ÜMIT <ubedretdin@gmail.com>
2019-11-03 15:01:49 +0200

    Tää printtaa ilmatieteen laitoksen avaruussään.

---
+++ avaruussää.py
@@ -0,0 +1,16 @@
+from urllib import request
+from bs4 import BeautifulSoup
+
+url = "https://ilmatieteenlaitos.fi/revontulet-ja-avaruussaa"
+html = request.urlopen(url).read().decode('utf8')
+
+soup = BeautifulSoup(html, 'html.parser')
+
+print("Kerron sinulle tämän päiväisen avaruussään. :\n")
+
+for p in soup.find_all('p'):
+    print(p.get_text())
+
+
+# Ongelma: miten filtteröidä hakukomentoa paremmin? Esimerkiks tää printtaa
+# kaikki 'p' tägätyt tekstit, vaikka vaan osa tosta saattaa on olennaista
