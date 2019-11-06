cb7926162573067672aa9f970df4cc7860fda18a
annen <nanneus@gmail.com>
2019-11-06 15:19:34 +0200

    #2 Koodia modattu!

--- avaruussää.py
+++ avaruussää.py
@@ -1,5 +1,6 @@
 from urllib import request
 from bs4 import BeautifulSoup
+import re
 
 url = "https://ilmatieteenlaitos.fi/revontulet-ja-avaruussaa"
 html = request.urlopen(url).read().decode('utf8')
@@ -9,21 +10,23 @@ soup = BeautifulSoup(html, 'html.parser')
 print("Kerron sinulle tämän päivän avaruussään:\n")
 
 # tulostetaan ensimmäinen otsikko, joka kertoo avaruussäästä:
-otsikot = [o.text for o in soup.find_all('h1')]
-print(otsikot[1])
+otsikot = [o for o in soup.find_all('h1')]
+print(otsikot[1].get_text())
 
 # tulostetaan teksti, joka koskee avaruussäätä:
-tekstit = [t.text for t in soup.find_all('p')]
+tekstit = [t for t in soup.find_all('p')]
 for t in tekstit[:2]:
-    print(t)
+    print(t.get_text().strip())
+
 
 # haetaan taulokosta revontulten todennäköisyys ja tulostetaan ne:
 taulukko = [t.text for t in soup.find_all('td')]
-aika = [line for line in taulukko[1:4]]
-saa = [line for line in taulukko[5:]]
+aika = [line.strip() for line in taulukko[1:4]]
+saa = [line.strip() for line in taulukko[5:]]
 rivit = [line for line in zip(aika, saa)]
 
-print(taulukko[4])
+
+print(taulukko[4].strip())
 for rivi in rivit:
     print(rivi[0] + ': ' + rivi[1])
 
