46d7ea6bccee9d5c317543370332ed4596e99663
Ümit <ubedretdin@gmail.com>
2019-11-20 20:23:52 +0200

    flask-hakumoottorin "luuranko."

--- flask-hakumoottori.py
+++ flask-hakumoottori.py
@@ -1,82 +1,12 @@
-from flask import Flask, render_template, request
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-import numpy as np
+from flask import Flask, render_template
 
-
-# Initialize flask instance
 app = Flask(__name__)
 
-# Luetaan tekstitiedosto kovalevyltä:
-
-def read_file():
-    try:
-        file_name = input("Where is your file?\n")
-        with open(file_name, encoding="utf8") as f:
-            documents = f.read().replace('\n', " ")
-            documents = documents.split(sep="</article>")
-    except PermissionError:
-        print("Incorrect path")
-    except FileNotFoundError:
-        print("Incorrect path")
-    except KeyError:
-        print("Incorrect path")
-    except SyntaxError:
-        print("Incorrect path")
-    return documents
-
-# Indeksoidaan sanaesiintymät
-
-documents = read_file()
-cv = CountVectorizer(lowercase=True, binary=True, token_pattern='(?u)\\b\\w+\\b', ngram_range=(1,2))
-sparse_matrix = cv.fit_transform(documents)
-sparse_td_matrix = sparse_matrix.T.tocsr()
-t2i = cv.vocabulary_
-gv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2", ngram_range=(1,2))
-g_matrix = gv.fit_transform(documents).T.tocsr()
+@app.route("/")
 
 
-def search_gutenberg(query_string):
-    # Vectorize query string
-    query_vec = gv.transform([query_string]).tocsc()
+def home():
+    return render_template("flask-haku.html")
 
-    # Cosine similarity
-    hits = np.dot(query_vec, g_matrix)
-
-    # Rank hits
-    ranked_scores_and_doc_ids = \
-        sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]),
-               reverse=True)
-
-    # Output result
-    print("Your query '{:s}' matches the following documents:".format(query_string))
-    for i, (score, doc_idx) in enumerate(ranked_scores_and_doc_ids):
-        print("Doc #{:d} (score: {:.4f}): {:s}".format(i, score, documents[doc_idx]))
-    print()
-
-d = {"and": "&", "AND": "&",
-     "or": "|", "OR": "|",
-     "not": "1 -", "NOT": "1 -",
-     "(": "(", ")": ")"}  # operator replacements
-
-
-def rewrite_query(query):  # rewrite every token in the query
-    return " ".join(rewrite_token(t) for t in query.split())
-
-def rewrite_token(t):
-    return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t))  # Make retrieved rows dense
-
-
-@app.route('/search')
-
-def search():
-    #get syote from URL variable
-    syote = request.args.get('syote')
-    #initialize list of matches
-    matches = []
-    # if syote exists
-    if syote:
-        for i, (score, doc_idx) in enumerate(ranked_scores_and_doc_ids):
-            matches.append()
-
-    return render_template('index.html')
-
+if __name__ == "__main__":
+    app.run(debug=True)
